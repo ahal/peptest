853caa10fd20049c48e11360611819d62c96867e
Andrew Halberstadt <halbersa@gmail.com>
2011-12-14 17:34:55 -0500

    Fix up some of the tests to use controller.rootElement

--- tests/firefox/examples/test_openBlankTab.js
+++ tests/firefox/examples/test_openBlankTab.js
@@ -43,11 +43,10 @@ c.open('http://mozilla.org');
 c.waitForPageLoad();
 
 // Only put things you want to test for responsiveness inside a perfom action call
-let page = findElement.ID(c.tabs.activeTab, "home");
 pep.performAction('open_blank_tab', function() {
-  page.keypress('t', {'ctrlKey': true});
+  c.windowElement.keypress('t', {'ctrlKey': true});
 });
 
 pep.performAction('close_blank_tab', function() {
-  page.keypress('w', {'ctrlKey': true});
+  c.windowElement.keypress('w', {'ctrlKey': true});
 });
--- tests/firefox/examples/test_openWindow.js
+++ tests/firefox/examples/test_openWindow.js
@@ -35,7 +35,6 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-// Import mozmill and initialize a controller
 Components.utils.import("resource://mozmill/driver/mozmill.js");
 let controller = getBrowserController();
 
